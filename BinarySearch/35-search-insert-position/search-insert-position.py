17e0df906f73210695e7e7ab9aac7af9ac8919dc
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-06-14 16:22:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (43.49%) - LeetSync

---
+++ BinarySearch/35-search-insert-position/search-insert-position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l =0;r=len(nums)-1
+        while l<=r:
+            m = l+(r-l)//2
+            if nums[m] == target: return m
+            if(target > nums[m]):
+                l = m+1
+            else: r = m-1
+        return l
