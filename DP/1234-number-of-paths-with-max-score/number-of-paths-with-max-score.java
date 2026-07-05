9bdd65e1c067919cb9b3415ef7a0887a7c9a4a15
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-07-06 04:46:06 +0530

    Time: 18 ms (32.18%) | Memory: 47.2 MB (19.54%) - LeetSync

--- DP/1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
+++ DP/1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -1,87 +1,63 @@
 class Solution {
-    int MOD = 1000000007;
-
+    int MOD  = 1000000007;
     class Pair{
         int sum;
         int path;
         Pair(int sum, int path){
             this.sum = sum;
             this.path = path;
-        }
+        } 
     }
-
     public int[] pathsWithMaxScore(List<String> board) {
-        int m = board.size();
-        int n = board.get(0).length();
-        Pair[][] dp = new Pair[m+1][n+1];
+        int r = board.size(), c=board.get(0).length();
+        Pair [][] dp = new Pair [r+1][c+1];
         dp[0][0] = new Pair(0,1);
-
-        for(int i=1;i<n;i++){
-            char ch = board.get(0).charAt(i);
+        for(int i=1;i<c;i++){
             Pair pre = dp[0][i-1];
-            if(ch == 'X' || pre.path == 0) dp[0][i] = new Pair(0,0);
-            else dp[0][i] = new Pair((ch-'0')+pre.sum,1);
+            char ch = board.get(0).charAt(i);
+            if(ch != 'X' && pre.path != 0){
+                dp[0][i] = new Pair(pre.sum +(ch-'0') , 1);
+            }else dp[0][i] = new Pair(0,0);
         }
-
-        for(int i=1;i<m;i++){
-            char ch = board.get(i).charAt(0);
+        for(int i=1;i<r;i++){
             Pair pre = dp[i-1][0];
-            if(ch == 'X' || pre.path == 0) dp[i][0] = new Pair(0,0);
-            else dp[i][0] = new Pair((ch-'0')+pre.sum,1);
+            char ch = board.get(i).charAt(0);
+            if(ch != 'X' && pre.path != 0){
+                dp[i][0] = new Pair(pre.sum +(ch-'0') , 1);
+            }else dp[i][0] = new Pair(0,0);
         }
 
-        for(int r= 1;r<m;r++){
-            char [] currRow = board.get(r).toCharArray();
-            for(int c = 1;c<n;c++){
-                int sum =0, path = 0;
-                if(currRow[c] == 'X'){
-                    dp[r][c] = new Pair(sum,path);
+        for(int i=1;i<r;i++){
+            char[] currRow = board.get(i).toCharArray();
+            for(int j=1;j<c;j++){
+                int sum = 0;
+                int path =0;
+                if(currRow[j] == 'X'){
+                    dp[i][j] = new Pair(sum,path);
                     continue;
                 }
-                int val =(currRow[c] =='S')?0: currRow[c] -'0';
-                int upSum = dp[r-1][c].sum;
-                int upPath = dp[r-1][c].path;
-                int leftSum = dp[r][c-1].sum;
-                int leftPath = dp[r][c-1].path;
-                int upLeftSum = dp[r-1][c-1].sum;
-                int upLeftPath = dp[r-1][c-1].path;
-                if(upSum == leftSum && upSum == upLeftSum){
-
-                    sum = val + upSum;
-                    path = (upPath + leftPath + upLeftPath)%MOD;
-
-                }else if(upSum == leftSum && upSum > upLeftSum){
-
-                    sum = val + upSum;
-                    path = (upPath + leftPath)%MOD;
-
-                }else if(upSum == upLeftSum && upSum > leftSum){
-
-                    sum = val + upSum;
-                    path = (upPath + upLeftPath)%MOD;
-
-                }else if(leftSum == upLeftSum && leftSum > upSum){
-
-                    sum = val +  leftSum;
-                    path = (leftPath + upLeftPath)%MOD;
-
-                }else{
-                    if(upSum > leftSum && upSum > upLeftSum){
-                        sum = val + upSum;
-                        path = upPath;
-                    }else if (leftSum > upLeftSum){
-                        sum = val+ leftSum;
-                        path = leftPath;
-                    }else{
-                        sum = val + upLeftSum;
-                        path = upLeftPath;
-                    }
+                Pair left = dp[i][j-1];
+                Pair up = dp[i-1][j];
+                Pair upLeft = dp[i-1][j-1];
+                int val =(currRow[j] != 'S')? currRow[j]-'0':0;
+                int best = Math.max(up.sum, Math.max(left.sum, upLeft.sum));
+                if (best == up.sum) {
+                     path = (path + up.path) % MOD;
                 }
-                if(path == 0) sum = 0;
-                dp[r][c] = new Pair(sum,path);
-            } 
+                if (best == left.sum) {
+                    path = (path + left.path) % MOD;
+                }
+                if (best == upLeft.sum) {
+                path = (path + upLeft.path) % MOD;
+                }
+                if (path != 0) {
+                    sum = val + best;
+                }
+                dp[i][j] = new Pair(sum,path);
+            }
         }
-        Pair res = dp[m-1][n-1];
-        return new int[]{res.sum,res.path};
+
+        Pair res = dp[r-1][c-1];
+        return new int[]{res.sum, res.path};
     }
 }
