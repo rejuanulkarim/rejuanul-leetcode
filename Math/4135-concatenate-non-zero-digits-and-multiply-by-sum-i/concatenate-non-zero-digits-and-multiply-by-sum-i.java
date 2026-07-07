b66c42d49b0ae2c168b932862762943448310bbf
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-07-07 13:43:53 +0530

    Time: 1 ms (99.85%) | Memory: 42.8 MB (18.68%) - LeetSync

--- Math/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ Math/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -4,12 +4,11 @@ class Solution {
         int sum =0,base=1;
         while(n>0){
             int r = n%10;
-            if(r!=0) {
-                num += base*r;
-                base *=10;
-            }
-            sum += r;
             n /=10;
+            if(r == 0) continue;
+            num += base*r;
+            base *=10;
+            sum += r;
         }
         return (long)sum * num;
     }
