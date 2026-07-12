fadd63bb9e618ed0e101d82871f241733c5d5423
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-07-12 18:24:19 +0530

    Time: 32 ms (51.36%) | Memory: 77 MB (31.99%) - LeetSync

--- HashTable/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ HashTable/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
         TreeSet<Integer> set = new TreeSet<>();
@@ -10,6 +11,28 @@ class Solution {
             map.put(num,rank++);
         }
         for(int i=0; i < arr.length ;i++ ){
+            arr[i] = map.get(arr[i]);
+        }
+        return arr;
+    }
+}
+*/
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+        int[] temp = new int[n];
+        for(int i=0;i<n;i++){
+            temp[i] = arr[i];
+        }
+        Arrays.sort(temp);
+        HashMap<Integer,Integer> map =new HashMap<>();
+        int rank =1;
+        for(int num:temp){
+            if(!map.containsKey(num)){
+                map.put(num,rank++);
+            }
+        }
+        for(int i=0;i<n;i++){
             arr[i] = map.get(arr[i]);
         }
         return arr;
