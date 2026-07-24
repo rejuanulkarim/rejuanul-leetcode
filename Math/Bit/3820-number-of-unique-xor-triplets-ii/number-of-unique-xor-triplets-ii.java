cd1e62c095b3a500c13cf0f5083392fe6c8e8331
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-07-24 19:42:33 +0530

    Time: 326 ms (45.45%) | Memory: 47.3 MB (5.45%) - LeetSync

--- Math/Bit/3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
+++ Math/Bit/3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
@@ -9,20 +9,24 @@ class Solution {
         int len =1;
         while(len <= max) len = len<<1;
         boolean [] pairXor = new boolean[len];
-        HashSet<Integer> res = new HashSet<>();
         
         for(int i=0;i<n;i++){
             for(int j=1;j<n;j++){
                 pairXor[nums[i]^nums[j]] = true;
             }
         }
+        boolean [] arr = new boolean[len];
+        int res = 0;
         for(int k=0;k<len;k++){
             if(pairXor[k]){
             for(int i=0;i<n;i++){
-                res.add(k ^ nums[i]);
+                int idx = k^nums[i];
+                if(! arr[idx]) res++;
+                arr[idx] = true;
+                
             }
             }
         }
-        return res.size();
+        return res;
     }
 }
