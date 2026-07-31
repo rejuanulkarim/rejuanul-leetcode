ef40845d883411a12a4a26528d327a3522ab4201
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-07-31 08:28:34 +0530

    Time: 9 ms (94.22%) | Memory: 48.3 MB (17.34%) - LeetSync

--- String/Greedy/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ String/Greedy/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,23 +1,19 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] dp = new int[27];
+        int[] dp = new int[26];
         for(char ch:word.toCharArray()){
             dp[ch-'a']++;
         }
         Arrays.sort(dp);
-        int idx = 26;
-        int flag = 1,press =1,res = 0,sum = 0;
-        while(dp[idx] > 0){
-            if(flag > 8 ){
-                res += press* sum;
-                sum = 0;
-                flag = 1;
+        int res =0;
+        for(int i = 25,count=1,press =1;i>=0;i--){
+            if(count > 8){
+                count =1;
                 press++;
             }
-            sum += dp[idx--];
-            flag++;
+            res += dp[i]*press;
+            count++;
         }
-        res += press* sum;
         return res;
     }
 }
