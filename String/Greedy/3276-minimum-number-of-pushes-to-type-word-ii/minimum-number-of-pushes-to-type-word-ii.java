70539ea8a90640a249c45c3754ccb87c182610d1
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-07-31 08:33:02 +0530

    Time: 9 ms (94.22%) | Memory: 48 MB (73.70%) - LeetSync

--- String/Greedy/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ String/Greedy/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,18 +1,21 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] dp = new int[26];
+        int[] freq = new int[26];
         for(char ch:word.toCharArray()){
-            dp[ch-'a']++;
+            freq[ch-'a']++;
         }
-        Arrays.sort(dp);
+        Arrays.sort(freq);
         int res =0;
         for(int i = 25,count=1,press =1;i>=0;i--){
+            /*
             if(count > 8){
                 count =1;
                 press++;
             }
-            res += dp[i]*press;
+            res += freq[i]*press;
             count++;
+            */
+            res += freq[i] *((25-i)/8 +1);
         }
         return res;
     }
