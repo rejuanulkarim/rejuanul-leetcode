21e57972697a5023de57b45b8f4c4526fc064188
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-08-04 00:15:28 +0530

    Time: 5 ms (95.17%) | Memory: 85.5 MB (65.91%) - LeetSync

--- DP/Math/1522-stone-game-iii/stone-game-iii.java
+++ DP/Math/1522-stone-game-iii/stone-game-iii.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     Integer dp[];
     public String stoneGameIII(int[] stoneValue) {
@@ -17,5 +18,21 @@ class Solution {
         if(i+1 < n) diff = Math.max(diff, nums[i]+nums[i+1] - solve(nums,i+2));
         if(i+2 < n) diff = Math.max(diff,nums[i]+nums[i+1]+nums[i+2] - solve(nums,i+3));
         return dp[i]=diff;
+    }
+}
+*/
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int dp[] = new int[n+1];
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--){
+            dp[i] = stoneValue[i] - dp[i+1];
+            if(i+2 <= n) dp[i] = Math.max(dp[i],stoneValue[i]+stoneValue[i+1] - dp[i+2]);
+            if(i+3 <= n) dp[i] = Math.max(dp[i],stoneValue[i]+stoneValue[i+1]+stoneValue[i+2] - dp[i+3]);
+        }
+        if(dp[0] < 0) return "Bob";
+        if(dp[0]> 0) return "Alice";
+        return "Tie";
     }
 }
