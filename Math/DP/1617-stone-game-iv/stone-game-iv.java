2f3d3065d13f0d76ca4a14053593503e6c7cd77c
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-08-11 00:04:21 +0530

    Time: 38 ms (48.82%) | Memory: 51 MB (30.03%) - LeetSync

--- Math/DP/1617-stone-game-iv/stone-game-iv.java
+++ Math/DP/1617-stone-game-iv/stone-game-iv.java
@@ -8,9 +8,7 @@ class Solution {
         if(n==0) return false;
         if(dp[n] != null) return dp[n];
         for(int i=1;i*i <= n;i++){
-            int m = n-(i*i);
-            dp[m] = solve(m);
-            if(! dp[m]) return true; 
+            if(!solve(n-i*i)) return dp[n] = true;
         }
         return dp[n] = false;
     }
