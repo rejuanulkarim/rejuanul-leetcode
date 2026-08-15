6de62336d09515b578ad9fbf60cbbd53ddbdd32a
Rejuanul Karim <rejuanulkarim1@gmail.com>
2026-08-15 21:34:28 +0530

    Time: 1 ms (100.00%) | Memory: 133.6 MB (34.56%) - LeetSync

--- BitManipulation/4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
+++ BitManipulation/4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int longestSubsequence(int[] nums) {
         int n = nums.length;
@@ -8,5 +9,20 @@ class Solution {
         }
         if(zero == n) return 0;
         return (xor == 0)? n-1:n;
+    }
+}
+*/
+
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int n = nums.length;
+        int zero = 0,xor=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0) zero++;
+            xor ^= nums[i];
+        }
+        if(xor != 0) return n;
+        else if(zero == n) return 0;
+        return n-1;
     }
 }
